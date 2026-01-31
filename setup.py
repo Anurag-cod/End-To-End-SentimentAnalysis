67d0a93378a4ee448742e2748fd92c7b3f6d1515
Anurag Ramteke <anurag932516@gmail.com>
2026-01-31 13:30:14 +0530

    updated docker file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="hate-speech-classification",
+    name="End-To-End-SentimentAnalysis",
     version="0.0.1",
-    author="iNeuron",
-    author_email="entbappy73@gmail.com",
+    author="Anurag",
+    author_email="anurag932516@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
