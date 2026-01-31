53df73086491093889f95ac12300bb3f3454c68c
Anurag Ramteke <anurag932516@gmail.com>
2026-01-31 12:17:43 +0530

    corrected app and pipeline name

--- app.py
+++ app.py
@@ -10,51 +10,51 @@ from hate.exception import CustomException
 from hate.constants import *
 
 
-# class PredictRequest(BaseModel):
-#     """Request body for 4-class sentiment prediction (Positive, Negative, Neutral, Irrelevant)."""
-#     text: str
+class PredictRequest(BaseModel):
+    """Request body for 4-class sentiment prediction (Positive, Negative, Neutral, Irrelevant)."""
+    text: str
 
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/", tags=["authentication"])
-# async def index():
-#     return RedirectResponse(url="/docs")
-
-
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
 
 
-# @app.get("/train")
-# async def training():
-#     try:
-#         train_pipeline = TrainPipeline()
 
-#         train_pipeline.run_pipeline()
 
-#         return Response("Training successful !!")
+@app.get("/train")
+async def training():
+    try:
+        train_pipeline = TrainPipeline()
 
-#     except Exception as e:
-#         return Response(f"Error Occurred! {e}")
+        train_pipeline.run_pipeline()
+
+        return Response("Training successful !!")
+
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
     
 
 
-# @app.post("/predict")
-# async def predict_route(req: PredictRequest):
-#     try:
-#         obj = PredictionPipeline()
-#         label = obj.run_pipeline(req.text)
-#         return {"sentiment": label}
-#     except Exception as e:
-#         raise CustomException(e, sys) from e
+@app.post("/predict")
+async def predict_route(req: PredictRequest):
+    try:
+        obj = PredictionPipeline()
+        label = obj.run_pipeline(req.text)
+        return {"sentiment": label}
+    except Exception as e:
+        raise CustomException(e, sys) from e
     
 
 
 
-# if __name__=="__main__":
-#     uvicorn.run(app, host=APP_HOST, port=APP_PORT)
+if __name__=="__main__":
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
 
 
     
-train_pipeline = TrainPipeline()
+# train_pipeline = TrainPipeline()
 
-train_pipeline.run_pipeline()
+# train_pipeline.run_pipeline()
